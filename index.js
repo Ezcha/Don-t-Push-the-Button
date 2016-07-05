b365858ea41624b0a901f2ffdaf05794b80e6f2d
Ezcha <felinecraft@gmail.com>
2016-07-05 00:37:20 -0500

    Update

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = 80;
+
 var seconds = 0;
 var highscore = 0;
 var users = 0;
-var counter;
+var mostUsers = 0;
+var pushCount = 0;
+var counter = 0;
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -17,11 +20,17 @@ http.listen(port, function(){
 	console.log('listening on *: '+port.toString());
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
 	users += 1;
+	if (users > mostUsers) {
+		mostUsers = users;
+		io.emit('mostUsers', users);
+	}
 	io.emit('time', seconds);
 	io.emit('highscore', highscore);
 	io.emit('users', users);
+	io.emit('mostUsers', users);
+	io.emit('pushCount', pushCount);
 	console.log('User connected. '+users.toString()+" users connected.");
 	socket.on('disconnect', function() {
 		users -= 1;
@@ -29,9 +38,12 @@ io.on('connection', function(socket){
 		console.log('User disconnected. '+users.toString()+" users connected.");
 	});
 	socket.on('push', function() {
+		console.log(socket.id);
 		console.log('Button has been pushed.');
 		seconds = 0;
+		pushCount += 1;
 		io.emit('time', seconds);
+		io.emit('pushCount', pushCount);
 		io.emit('push');
 		clearInterval(counter);
 		counter = setInterval(function() {
